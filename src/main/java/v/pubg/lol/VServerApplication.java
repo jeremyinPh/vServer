edff8d77117053f71e63dc13a42a358bf33ac72b
Jeremy <Jeremy@leegame.net>
2020-02-15 16:49:19 +0800

    project init

---
+++ src/main/java/v/pubg/lol/VServerApplication.java
@@ -0,0 +1,13 @@
+package v.pubg.lol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/v/pubg/lol/VServerApplicationTests.java
@@ -0,0 +1,13 @@
+package v.pubg.lol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
